29714f461db7e7fc1279b78fab7bb684e74a55e1
petervargaofficial <petervarga@Peters-MacBook-Pro.local>
2014-05-06 22:12:36 -0700

    First commit on trackSnack

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrackSnack::Application.config.secret_key_base = 'bf6c94414d096f0331d96d31564b7c3ea75f44b4edfaec21e2c6412f1c42f4ccfddf3a5d8938ebdf04845e20a4c84301d630cc74510fe6dc73f02a1b0423586a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TrackSnack::Application.config.session_store :cookie_store, key: '_trackSnack_session'
