9b64baf729bf7b6403a288f871a611755a8cdb6b
petervargaofficial <petervarga@Peters-MacBook-Pro.local>
2014-05-07 13:24:18 -0700

    site controller, route. hello world style of app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # PAST SEEDs
 # Vendor.create(name: "John's burger", latitude: 37.779691, longitude: -122.394849)
 # Vendor.create(name: "Simona's taco", latitude: 37.779697, longitude: -122.394840)
+# MenuItem.create(item_name: "Perfect burger",item_description: "The most amazing burger on planet earth!", item_price: 1000, vendor_id: 2)
+# MenuItem.create(item_name: "Perfect Taco",item_description: "The most amazing Taco on planet earth!", item_price: 500, vendor_id: 3)
 
-MenuItem.create(item_name: "Perfect burger",item_description: "The most amazing burger on planet earth!", item_price: 1000, vendor_id: 2)
-MenuItem.create(item_name: "Perfect Taco",item_description: "The most amazing Taco on planet earth!", item_price: 500, vendor_id: 3)
