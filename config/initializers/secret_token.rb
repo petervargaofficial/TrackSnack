e080420846c4dcaf688cf68765c7151d4aed8249
petervargaofficial <petervarga@Peters-MacBook-Pro.local>
2014-05-07 08:31:48 -0700

    Hiding the secrets using a ENV hash. Forcing ssl in production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-TrackSnack::Application.config.secret_key_base = 'bf6c94414d096f0331d96d31564b7c3ea75f44b4edfaec21e2c6412f1c42f4ccfddf3a5d8938ebdf04845e20a4c84301d630cc74510fe6dc73f02a1b0423586a'
+TrackSnack::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
