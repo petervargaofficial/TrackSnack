5a9aa9289c5bc93e26edcb4d5d9669770a1f95b4
petervargaofficial <petervarga@Peters-MacBook-Pro.local>
2014-05-07 14:20:50 -0700

    working on controller and views for vendors

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 TrackSnack::Application.routes.draw do
   root 'sites#index'
+  resources :vendors
 end
+
+
+# Peters-MacBook-Pro:trackSnack petervarga$ rake routes
+#      Prefix Verb   URI Pattern                 Controller#Action
+#        root GET    /                           sites#index
+#     vendors GET    /vendors(.:format)          vendors#index
+#             POST   /vendors(.:format)          vendors#create
+#  new_vendor GET    /vendors/new(.:format)      vendors#new
+# edit_vendor GET    /vendors/:id/edit(.:format) vendors#edit
+#      vendor GET    /vendors/:id(.:format)      vendors#show
+#             PATCH  /vendors/:id(.:format)      vendors#update
+#             PUT    /vendors/:id(.:format)      vendors#update
+#             DELETE /vendors/:id(.:format)      vendors#destroy
