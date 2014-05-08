3dca84fb3d3d18897749cf683603b7c19b4a2198
petervargaofficial <petervarga@Peters-MacBook-Pro.local>
2014-05-07 17:45:22 -0700

    vendor creation and editing partialy finished

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,4 +1,34 @@
 class VendorsController < ApplicationController
 	def index
+		@vendors = Vendor.all
 	end
+
+	def new
+		@vendor = Vendor.new
+	end
+
+	def create
+		vendor = Vendor.create vendor_params
+		redirect_to(vendor)
+	end
+
+	def show
+		@vendor = Vendor.find_by_id(params[:id])
+	end
+
+	def edit
+		@vendor = Vendor.find_by_id(params[:id])
+	end
+
+	def destroy
+		Vendor.find_by_id(params[:id]).delete
+		redirect_to vendors_path
+	end
+
+	private
+
+	def vendor_params
+		params.require(:vendor).permit(:name, :logo, :latitude, :longitude)
+	end
+
 end
